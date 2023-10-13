4e3ffad8d93eea6c90d07f9bb8724eea1b3afc3a
SengokuGarp <garpirungu@gmail.com>
2023-10-13 11:35:05 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,31 @@
 const init = () => {
+    const inputForm = document.querySelector('form');
+    const movieDetailsTitle = document.querySelector('#movieDetails h4');
+    const movieDetailsSummary = document.querySelector('#movieDetails p');
   
-}
-
-document.addEventListener('DOMContentLoaded', init);
+    inputForm.addEventListener('submit', (event) => {
+      event.preventDefault();
+      const input = document.querySelector('input#searchByID');
+      const movieId = input.value;
+  
+      fetch(`http://localhost:4000/movies/${movieId}`)
+        .then(response => {
+          if (response.status === 404) {
+            throw new Error('Movie not found');
+          }
+          return response.json();
+        })
+        .then(data => {
+          movieDetailsTitle.innerText = data.title;
+          movieDetailsSummary.innerText = data.summary;
+        })
+        .catch(error => {
+          console.log(error);
+          movieDetailsTitle.innerText = 'Movie not found';
+          movieDetailsSummary.innerText = '';
+        });
+    });
+  }
+  
+  document.addEventListener('DOMContentLoaded', init);
+  
